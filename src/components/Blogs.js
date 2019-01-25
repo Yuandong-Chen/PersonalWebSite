0af70fcbf36e4f16b941ea029d929ab61bc4a17f
Yuandong-Chen <YuandongChen@macair.local>
2019-01-24 23:39:25 -0500

    finish blog proto

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -15,29 +15,44 @@ const Extract = styled.span`
 `;
 
 const CardFrame = styled.div`
+  flex: 1 1 50px;
   border: solid 1px blue;
-  margin-top: 5px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: ${props => props.toggled? "row": "column"};
+  flex-wrap: wrap;
+  justify-content: space-between;
+  border: solid 1px blue;
 `;
 
 const BlogCard = ({title, extract, date, _rating, ...rest}) => {
   return (
-    <CardFrame {...rest}>
-      <Title>{title}</Title>
-      <span>{date}</span>
-      <Extract>{extract}</Extract>
+    <CardFrame title={title} {...rest}>
+    <Flipped inverseFlipId={title}>
+      <Title title={title}>{title}</Title>
+    </Flipped>
+      <span title={title}>{date}</span>
+      <Extract title={title}>{extract}</Extract>
       {_rating}
     </CardFrame>
   );
 };
 
-const ExpandedBlog = ({title, data, date}) => {
-
+const ExpandedBlog = ({title, data, date, onClick}) => {
+  return <Flipped flipId={title}>
+  <div onClick={onClick}>
+  <Title>{title} {date}</Title>
+  {data}
+  </div>
+  </Flipped>
 };
 
 class Blogs extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searching: "", articles: articles.slice().sort((a, b) => {
+    this.state = {inFullScreen:null, toggled:false, searching: "", articles: articles.slice().sort((a, b) => {
       if(b.date > a.date) return 1;
       if(a.date == b.date) {
         if(a.title < b.date) {
@@ -49,32 +64,52 @@ class Blogs extends React.Component {
     })};
   }
 
+  setToggle = () => {
+    this.setState(({toggled}) => ({toggled: !toggled}));
+  }
+
+  setFullScreen = (s) => {
+    s.preventDefault();
+    s.stopPropagation();
+    let i = s.target.title;
+    this.setState(({isFullScreen}) => ({isFullScreen: i}));
+  }
+
+  onCancel = () => {
+    this.setState(({isFullScreen}) => ({isFullScreen: null}));
+  }
+
   setSearching = (s) => {
     //we need to modify the rating of our articles
     this.setState({ searching: s.target.value, articles: rating(s.target.value, this.state.articles.slice()) });
   }
 
-  RawHTML({children, className = ""}){
-    return <div className={className} dangerouslySetInnerHTML={{ __html: children.replace(/\n/g, '<br />')}} />;
+  RawHTML(children){
+    return <div dangerouslySetInnerHTML={{ __html: children.replace(/\n/g, '<br />')}} />;
   }
 
   render() {
     return (
-      <div>
-      <Flipper flipKey={this.state.searching} spring="gentle">
-      <Search onChange={this.setSearching} />
+      <Flipper flipKey={this.state.searching+this.state.toggled+this.state.isFullScreen} spring="stiff">
+      {this.state.isFullScreen?
+      this.state.articles.filter((x) => (x.title == this.state.isFullScreen))
+      .map((obj) => {return <ExpandedBlog key={obj.title} onClick={this.onCancel} title={obj.title} data={this.RawHTML(obj.data)} date={obj.date} />})
+      :<div><Search onChange={this.setSearching} onClick={this.setToggle} />
+      <Wrapper toggled={this.state.toggled} onClick={this.setFullScreen}>
       {
         this.state.articles.map((obj) =>
-        <Flipped key={obj.title} flipId={obj.title}>
+        <Flipped key={obj.title} flipId={obj.title} stagger={true}>
           <BlogCard
             key={obj.title}
             title={obj.title}
             extract={obj.extract}
-            date={obj.date} _rating={obj._rating}/>
+            date={obj.date}
+            _rating={obj._rating}
+            />
         </Flipped>)
       }
+      </Wrapper></div>}
       </Flipper>
-      </div>
     );
   }
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import {Card} from 'styled-icons/boxicons-regular';
 
 const Wrapper = styled.div`
   display: flex;
@@ -8,7 +9,15 @@ const Wrapper = styled.div`
   border: solid 1px red;
 `;
 
+const Toggle = styled.button`
+  flex: 1 0
+  text-align: right;
+  background: none;
+  max-width: 60px;
+`;
+
 const Input = styled.input`
+  flex: 1 0
   height: 24px;
   padding: 0.5em;
   margin: 0.5em;
@@ -17,10 +26,13 @@ const Input = styled.input`
   background: papayawhip;
   border: none;
   border-radius: 3px;
+  max-width: 100px;
 `;
 
-const Search = ({onChange}) => {
-  return <Wrapper><Input type="text" placeholder="Search" onChange={onChange} /></Wrapper>
+const Search = ({onChange, onClick}) => {
+  return <Wrapper>
+          <Toggle onClick={onClick}><Card size="45px"/></Toggle><Input type="text" placeholder="Search" onChange={onChange} />
+        </Wrapper>
 };
 
 export default Search;
